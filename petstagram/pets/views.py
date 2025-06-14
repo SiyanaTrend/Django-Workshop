c6f49338f9e80be12b40a8bb1da39efddd8bea40
Siyana Trendafilova <bebovska@gmail.com>
2025-06-14 20:21:26 +0300

    feat: Add the URLs (paths) and views

--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,3 +1,17 @@
+from urllib.request import Request
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def pet_add_view(request: Request) -> HttpResponse:
+    return render(request, 'pets/pet-add-page.html')
+
+def pet_details_view(request: Request, username: str, pet_slug: str) -> HttpResponse:
+    return render(request, 'pets/pet-details-page.html')
+
+def pet_edit_view(request: Request, username: str, pet_slug: str) -> HttpResponse:
+    return render(request, 'pets/pet-edit-page.html')
+
+def pet_delete_view(request: Request, username: str, pet_slug: str) -> HttpResponse:
+    return render(request, 'pets/pet-delete-page.html')
+
--- petstagram/photos/views.py
+++ petstagram/photos/views.py
@@ -1,3 +1,14 @@
+from urllib.request import Request
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def photo_add_view(request: Request) -> HttpResponse:
+    return render(request, 'photos/photo-add-page.html')
+
+def photo_details_view(request: Request, pk: int) -> HttpResponse:
+    return render(request, 'photos/photo-details-page.html')
+
+def photo_edit_view(request: Request, pk: int) -> HttpResponse:
+    return render(request, 'photos/photo-edit-page.html')
+
