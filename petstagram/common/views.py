792466a1be94d05142656f9b655ff2be838ad783
Siyana Trendafilova <bebovska@gmail.com>
2025-06-19 15:03:59 +0300

    feat: Create home-page, pet-details fuctionality

--- petstagram/common/views.py
+++ petstagram/common/views.py
@@ -3,7 +3,15 @@ from urllib.request import Request
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from photos.models import Photo
+
+
 def home_page_view(request: Request) -> HttpResponse:
-    return render(request, 'common/home-page.html')
+    all_photos = Photo.objects.prefetch_related('tagged_pets').all()
+
+    context = {
+        'all_photos': all_photos,
+    }
+    return render(request, 'common/home-page.html', context)
 
 
