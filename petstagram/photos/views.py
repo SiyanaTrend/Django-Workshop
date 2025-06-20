5f0e07149164117ce9b2c5872b685307edf587d3
Siyana Trendafilova <bebovska@gmail.com>
2025-06-21 00:40:59 +0300

    feat: Add CBV solutions in photo app

--- petstagram/photos/views.py
+++ petstagram/photos/views.py
@@ -1,51 +1,82 @@
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from common.forms import CommentForm
 from photos.forms import PhotoCreateForm, PhotoEditForm
 from photos.models import Photo
 
 
-def photo_add_view(request: HttpRequest) -> HttpResponse:
-    form = PhotoCreateForm(request.POST or None, request.FILES or None)
-
-    if request.method == 'POST' and form.is_valid():
-        form.save()
-        return redirect('home-page')
-
-    context = {
-        'form': form,
-    }
-
-    return render(request, 'photos/photo-add-page.html', context)
+class PhotoAddView(CreateView):
+    model = Photo
+    form_class = PhotoCreateForm
+    template_name = 'photos/photo-add-page.html'
+    success_url = reverse_lazy('home-page')
 
 
-def photo_details_view(request: HttpRequest, pk: int) -> HttpResponse:
-    photo = Photo.objects.get(pk=pk)
-    comments = photo.comment_set.all()
-    comment_form = CommentForm()
+# def photo_add_view(request: HttpRequest) -> HttpResponse:
+#     form = PhotoCreateForm(request.POST or None, request.FILES or None)
+#
+#     if request.method == 'POST' and form.is_valid():
+#         form.save()
+#         return redirect('home-page')
+#
+#     context = {
+#         'form': form,
+#     }
+#
+#     return render(request, 'photos/photo-add-page.html', context)
 
-    context = {
-        'photo': photo,
-        'comments': comments,
-        'comment_form': comment_form,
-    }
 
-    return render(request, 'photos/photo-details-page.html', context)
+class PhotoDetailsView(DetailView):
+    model = Photo
+    template_name = 'photos/photo-details-page.html'
 
-def photo_edit_view(request: HttpRequest, pk: int) -> HttpResponse:
-    photo = Photo.objects.get(pk=pk)
-    form = PhotoEditForm(request.POST or None, instance=photo)
+    def get_context_data(self, **kwargs):
+        kwargs.update({
+            'comments': self.object.comment_set.all(),
+            'comment_form': CommentForm(),
+        })
+        return super().get_context_data(**kwargs)
 
-    if request.method == 'POST' and form.is_valid():
-        form.save()
-        return redirect('photo-details', pk=pk)
 
-    context = {
-        'form': form,
-    }
+# def photo_details_view(request: HttpRequest, pk: int) -> HttpResponse:
+#     photo = Photo.objects.get(pk=pk)
+#     comments = photo.comment_set.all()
+#     comment_form = CommentForm()
+#
+#     context = {
+#         'photo': photo,
+#         'comments': comments,
+#         'comment_form': comment_form,
+#     }
+#
+#     return render(request, 'photos/photo-details-page.html', context)
 
-    return render(request, 'photos/photo-edit-page.html', context)
+
+class PhotoEditView(UpdateView):
+    model = Photo
+    form_class = PhotoEditForm
+    template_name = 'photos/photo-edit-page.html'
+
+    def get_success_url(self):
+        return reverse('photo-details', kwargs={'pk': self.object.pk})
+
+
+# def photo_edit_view(request: HttpRequest, pk: int) -> HttpResponse:
+#     photo = Photo.objects.get(pk=pk)
+#     form = PhotoEditForm(request.POST or None, instance=photo)
+#
+#     if request.method == 'POST' and form.is_valid():
+#         form.save()
+#         return redirect('photo-details', pk=pk)
+#
+#     context = {
+#         'form': form,
+#     }
+#
+#     return render(request, 'photos/photo-edit-page.html', context)
 
 
 def photo_delete_view(request: HttpRequest, pk: int) -> HttpResponse:
