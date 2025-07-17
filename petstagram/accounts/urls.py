8b72f41cff97f979a6e9d02ab19009cdadfc1628
Siyana Trendafilova <bebovska@gmail.com>
2025-07-18 00:25:15 +0300

    feat: Add logout functionality

--- petstagram/accounts/urls.py
+++ petstagram/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
 from accounts import views
@@ -6,6 +6,7 @@ from accounts import views
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='accounts/login-page.html'), name='login'),
+    path("logout/", LogoutView.as_view(), name='logout'),
     path('profile/<int:pk>/', include([
         path('', views.profile_details_view, name='profile-details'),
         path('edit/', views.profile_edit_view, name='edit-profile'),
