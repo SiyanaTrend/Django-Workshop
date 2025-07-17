834abd03e14070da746f9161a5fc27efd3b55bae
Siyana Trendafilova <bebovska@gmail.com>
2025-07-17 23:05:24 +0300

    feat: Add profile model and admin functionality

--- petstagram/accounts/models.py
+++ petstagram/accounts/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 from accounts.managers import AppUserManager
 
 
-class AppUser(AbstractBaseUser):
+class AppUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(
         unique=True,
     )
@@ -20,3 +21,33 @@ class AppUser(AbstractBaseUser):
     objects = AppUserManager()
     def __str__(self):
         return self.email
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        AppUser,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    first_name = models.CharField(
+        max_length=30,
+        blank=True,
+        null=True,
+    )
+
+    last_name = models.CharField(
+        max_length=30,
+        blank=True,
+        null=True,
+    )
+
+    date_of_birth = models.DateField(
+        blank=True,
+        null=True,
+    )
+
+    profile_picture = models.URLField(
+        blank=True,
+        null=True,
+    )
