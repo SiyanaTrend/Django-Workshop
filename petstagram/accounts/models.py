c90767c5135b6598c61d417d7136ced120ea37fd
Siyana Trendafilova <bebovska@gmail.com>
2025-07-17 22:28:30 +0300

    feat: Create a custom user model inheriting from the AbstractBaseUser Django class

--- petstagram/accounts/models.py
+++ petstagram/accounts/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+from accounts.managers import AppUserManager
+
+
+class AppUser(AbstractBaseUser):
+    email = models.EmailField(
+        unique=True,
+    )
+    is_active = models.BooleanField(
+        default=True,
+    )
+    is_staff = models.BooleanField(
+        default=False,
+    )
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = AppUserManager()
+    def __str__(self):
+        return self.email
