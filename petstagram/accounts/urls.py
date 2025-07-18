9a482774b1bb8adee8f34dd912d7d91659b38959
Siyana Trendafilova <bebovska@gmail.com>
2025-07-19 01:47:27 +0300

    feat: Add profile delete functionality

--- petstagram/accounts/urls.py
+++ petstagram/accounts/urls.py
@@ -6,10 +6,10 @@ from accounts import views
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='accounts/login-page.html'), name='login'),
-    path("logout/", LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/<int:pk>/', include([
-        path('', views.profile_details_view, name='profile-details'),
+        path('', views.ProfileDetailView.as_view(), name='profile-details'),
         path('edit/', views.ProfileEditView.as_view(), name='edit-profile'),
-        path('delete/', views.profile_delete_view, name='delete-profile')
+        path('delete/', views.app_user_delete_view, name='delete-profile')
     ])),
 ]
